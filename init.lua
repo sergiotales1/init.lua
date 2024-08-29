6373d2cf7b462c8e42f54d4a7401a8c0260e085e
SergioTales1 <algumemailai3@gmail.com>
2024-08-29 09:39:00 -0300

    autopairs installed

--- init.lua
+++ init.lua
@@ -2,4 +2,8 @@
 require("config.lazy")
 -- set handlebars ext to be like html
 vim.cmd("autocmd BufRead,BufNewFile *.handlebars set filetype=html")
+require("kanagawa").setup({
+  transparent = true,
+  theme = "dark",
+})
 vim.cmd("colorscheme kanagawa-wave")
